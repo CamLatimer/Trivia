921a576d675064ab12d53682ad24c8d077a707c5
camlatimer <camlatimer@gmail.com>
2017-11-23 11:56:20 -0500

    QuestionsContainer and Question components handle answer selection: disable buttons on selection and show correct answer

--- client/js/Question.js
+++ client/js/Question.js
@@ -1,36 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// this component shows the trivia question data
 export default class Question extends React.Component {
 
       constructor(props){
         super(props);
-        this.state = {
-          wasAnswerSubmitted: false
-        }
-        this.showCorrectAnswer = this.showCorrectAnswer.bind(this);
+        this.state = {};
+        this.handleAnswerSelection = this.handleAnswerSelection.bind(this);
       }
 
-      showCorrectAnswer(e){
-        e.preventDefault();
-        console.log(this.props.correctAnswer);
-        this.setState({
-          wasAnswerSubmitted: true
-        })
+      // run when a trivia answer is selected.
+      // enable radio buttons and update the state with the correct answer.
+      // when selection is made, state gets updated with correct answer.
+      // updating the state with the correct answer signals that a selection has been made.
+      // if state is updated with correct answer, disable the radio buttons.
+      // (todo: come up with a way to make that less complex?)
+      handleAnswerSelection(e){
+        this.props.checkRadio(e);
+        if(this.state.correctAnswer === this.props.correctAnswer){
+          e.preventDefault();
+          e.target.disabled = true;
+        } else {
+          this.setState(() => ({
+            correctAnswer: this.props.correctAnswer
+          }))
+        }
+      }
+
+      componentDidMount(){
+
       }
 
       render(){
         let correctAnswer = null;
-        if(this.state.wasAnswerSubmitted){
-          correctAnswer = <div>{this.props.correctAnswer}</div>
+        // conditonal is a way to check if an answer has been selected
+        if(this.state.correctAnswer === this.props.correctAnswer){
+          correctAnswer = <div>{this.state.correctAnswer}</div>
         } else {
           correctAnswer = null;
         }
-        let wrongAnswers = this.props.wrongAnswers.map((answer, index) => {
+
+        // map the answer options out into radio buttons
+        let answersArray = this.props.answersArray.map((answer, index) => {
           return (
             <label key={index}>
               <div>
-                <input type="radio" name="answer" value="answer" />
+                <input
+                  checked={this.props.isRadioChecked}
+                  onClick={this.handleAnswerSelection}
+                  type="radio" name={`answer${this.props.answerIndex}`} value={answer} />
               {answer}
               </div>
             </label>
@@ -38,14 +57,13 @@ export default class Question extends React.Component {
         });
 
         return(
-          <form>
-            {this.props.question}
+          <div>
+              {this.props.question}
             <div>
-              {wrongAnswers}
-             </div>
-             {correctAnswer}
-             <button onClick={this.showCorrectAnswer}>SubmitAnswer</button>
-          </form>
+              {answersArray}
+            </div>
+              {correctAnswer}
+          </div>
         );
       }
 
