6311a4d19e42eec4ed17a71cd7f5ea5bb054e6ed
camlatimer <camlatimer@gmail.com>
2017-12-02 15:55:28 -0500

    adds a general error handler to middleware

--- index.js
+++ index.js
@@ -9,11 +9,20 @@ db.dbConnect();
 
 // serve static files from /dist, use '/dist' as a mount path for proxies, middleware, etc.
 app.use('/dist', express.static('dist'))
+app.set('view engine', 'ejs');
 
+// include routes
 app.use('/', pages);
 app.use('/api', api);
 
-app.set('view engine', 'ejs');
+// error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 app.listen(process.env.PORT || 8080, () => {
     console.log('The application is running on localhost:3000!')
